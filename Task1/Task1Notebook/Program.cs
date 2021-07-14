0a8cf52595c5b45cdd5bebc60dd2c7f1acca5dc9
Jessie Houghton <t-jhoughton@microsoft.com>
2021-07-14 14:17:36 -0500

    Added notebook

---
+++ Task1/Task1Notebook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1Notebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
